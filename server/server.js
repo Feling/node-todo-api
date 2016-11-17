5df8dcb8abe4865fed07034c6448fd52c1e9dd3c
igork <feling87@gmail.com>
2016-11-17 13:10:11 +0200

    Adding triming

--- server/server.js
+++ server/server.js
@@ -4,34 +4,53 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo', {
-   text: {
-       type: String
-   } ,
+    text: {
+        type: String,
+        require: true,
+        minlength: 1,
+        trim: true
+    },
     completed: {
-       type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-       type: Number
+        type: Number,
+        default: null
+    }
+});
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        trim: true,
+        require: true,
+        minlength: 1
+
     }
 });
 
 let newTodo = new Todo({
-   text: 'Cook diner'
+    text: 'Cook diner'
 });
 
-// newTodo.save().then((doc)=> {
-//     console.log('Saved todo', doc)
-// }, (e)=>{
-//     console.log('Unable to save')
+//
+// let otherTodo = new Todo({
+//     text: ' Feed the cat    ',
+//     completed: true,
+//     completedAt: 123
+// });
+// otherTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log(e);
 // });
 
-let otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
+let user = new User({
+    email: "  me@com.com  "
 });
-otherTodo.save().then((doc)=> {
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (e)=>{
+user.save().then((user)=> {
+    console.log(JSON.stringify(user, undefined, 2));
+},
+(e) => {
     console.log(e);
 });
