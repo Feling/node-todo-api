c2c598a0f21c1990b5624b77b367fe0f085cf647
igork <feling87@gmail.com>
2016-11-17 13:45:56 +0200

    Refactoring models

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require(mongoose);
+
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        trim: true,
+        require: true,
+        minlength: 1
+
+    }
+});
+
+module.exports = {user};
